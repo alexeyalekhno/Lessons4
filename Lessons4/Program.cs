9df7ac94330d0e46b40e165cf57746aa5296871b
mrale <mrale@DESKTOP-HHS76V9>
2024-11-23 19:55:49 +0300

    Добавьте файлы проекта.

---
+++ Lessons4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите N - ");
+            uint N = uint.Parse(Console.ReadLine());
+            long squareN = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                squareN += 2 * i - 1;
+                Console.WriteLine("{0}^2 - {1}", i, squareN);
+            }
+            Console.ReadKey();
+        }
+    }
+}
